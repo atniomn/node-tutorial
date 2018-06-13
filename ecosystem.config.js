922e570f76dcd39db34517a9e9302dab3427e1d0
Tim <tim_mcinerney@hotmail.com>
2018-06-13 00:07:23 -0500

    add eco system

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports= {
+	apps: [{
+		name: 'tutorial-2',
+		script: './index.js'
+	}],
+	deploy: {
+		production: {
+			user: 'ubuntu',
+			host: '18.222.48.98',
+			key: '~/.ssh/aws_key.pem',
+			ref: 'origin/master',
+			repo: 'git@github.com:atniomn/node-tutorial.git',
+			path: '/home/ubuntu/server',
+			'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+		}
+	}
+}
